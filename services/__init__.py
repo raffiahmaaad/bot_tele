679254cf47cf4e8c93375c38ba66d2804d087962
raffiahmaaad <raffi.ahmaaaddd@gmail.com>
2026-01-21 19:59:05 +0700

    Fix: Remove non-existent pakasir_client import that caused bot loading failure

--- services/__init__.py
+++ services/__init__.py
@@ -1,15 +1,12 @@
 """Services package."""
-from services.pakasir import pakasir_client, PakasirClient, PaymentResponse, TransactionStatus
+from services.pakasir import PakasirClient, PaymentResponse, TransactionStatus
 from services.delivery import deliver_product
 
 # Note: SheerID service is imported separately via services.sheerid
 
 __all__ = [
-    "pakasir_client",
     "PakasirClient", 
     "PaymentResponse",
     "TransactionStatus",
     "deliver_product",
 ]
-
-
